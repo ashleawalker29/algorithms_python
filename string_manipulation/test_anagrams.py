988b18da8072d7251413b761c1417927c9e03f3e
ashleawalker29 <ashleawalker29@gmail.com>
2020-06-08 11:42:40 -0600

    Created anagram tests.

---
+++ string_manipulation/test_anagrams.py
@@ -0,0 +1,20 @@
+import unittest
+
+from anagrams import is_anagram
+
+
+class AnagramsTests(unittest.TestCase):
+
+    def test_anagrams_none(self):
+        self.assertEqual(is_anagram(None, 'Example'),
+                         'Cannot find an anagram from an empty string.')
+        self.assertEqual(is_anagram('Example', None),
+                         'Cannot find an anagram from an empty string.')
+        self.assertEqual(is_anagram(None, None),
+                         'Cannot find an anagram from an empty string.')
+
+    def test_anagrams_partial(self):
+        self.assertEqual(is_anagram('The army', 'Mary'), 'Partial anagram.')
+
+    def test_anagrams_full(self):
+        self.assertEqual(is_anagram('Oh, lame saint.', 'The Mona Lisa'), 'Full anagrams.')
