ad1fdaec9da050180166b2edcd828d1ad5c598b0
ashleawalker29 <ashleawalker29@gmail.com>
2020-06-17 15:13:08 -0600

    Renamed BST class to BSTNode for clarity.

--- bst/bst.py
+++ bst/bst.py
@@ -1,4 +1,4 @@
-class BST():
+class BSTNode():
     def __init__(self, root):
         self.root = root
         self.left_branch = None
--- bst/test_bst.py
+++ bst/test_bst.py
@@ -1,12 +1,13 @@
 import unittest
 
-from bst import BST
+from bst import BSTNode
 
 
 class BinarySearchTreeTests(unittest.TestCase):
 
     def test_bst_attributes(self):
-        tree = BST(1)
+        tree = BSTNode(1)
         self.assertEqual(tree.root, 1)
         self.assertEqual(tree.left_branch, None)
         self.assertEqual(tree.right_branch, None)
+
