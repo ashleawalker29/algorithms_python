f4f9369208fda4992ecf4508f13a01e5bff021c9
ashleawalker29 <ashleawalker29@gmail.com>
2020-06-17 15:41:40 -0600

    Implemented BST validation method.
    Fixed test_is_bst bst logic as it was not a valid bst before.

--- bst/test_bst.py
+++ bst/test_bst.py
@@ -3,6 +3,22 @@ import unittest
 from bst import BSTNode
 
 
+def is_BST(root):
+    evaluated_nodes = []
+    previous_node = None
+
+    while root or evaluated_nodes:
+        while root:
+            evaluated_nodes.append(root)
+            root = root.left_branch
+        root = evaluated_nodes.pop()
+        if previous_node and root.root <= previous_node.root:
+            return False
+        previous_node = root
+        root = root.right_branch
+    return True
+
+
 class BinarySearchTreeTests(unittest.TestCase):
 
     def test_bst_attributes(self):
@@ -14,7 +30,7 @@ class BinarySearchTreeTests(unittest.TestCase):
     def test_is_bst(self):
         tree = BSTNode(2)
         tree.left_branch = BSTNode(1)
-        tree.right_branch = BSTNode(2)
+        tree.right_branch = BSTNode(3)
 
         self.assertTrue(is_BST(tree))
 
