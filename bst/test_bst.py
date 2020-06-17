c834cb074c6cae705bd0bf920dbf69dcb2e2d77f
ashleawalker29 <ashleawalker29@gmail.com>
2020-06-17 15:36:24 -0600

    Added tests for bst validation.

--- bst/test_bst.py
+++ bst/test_bst.py
@@ -11,3 +11,16 @@ class BinarySearchTreeTests(unittest.TestCase):
         self.assertEqual(tree.left_branch, None)
         self.assertEqual(tree.right_branch, None)
 
+    def test_is_bst(self):
+        tree = BSTNode(2)
+        tree.left_branch = BSTNode(1)
+        tree.right_branch = BSTNode(2)
+
+        self.assertTrue(is_BST(tree))
+
+    def test_is_not_bst(self):
+        tree = BSTNode(3)
+        tree.left_branch = BSTNode(2)
+        tree.right_branch = BSTNode(1)
+
+        self.assertFalse(is_BST(tree))
