04768857608d6f18e1f4b06bd2eb89686d128807
ashleawalker29 <ashleawalker29@gmail.com>
2020-06-18 15:26:00 -0600

    Removed wedding items from the evaluation list.
    These were removed due to the fact that they will never be accessed because of the
    way the list is sorted for evaluation.

--- math/acnh/WeddingItems.py
+++ math/acnh/WeddingItems.py
@@ -12,15 +12,8 @@ class WeddingItem():
 
 wedding_items = [
     WeddingItem(name='Wedding Arch', hcr=20, sell_price=5000),
-    WeddingItem(name='Wedding Bench', hcr=5, sell_price=875),
-    WeddingItem(name='Wedding Cake', hcr=5, sell_price=1000),
     WeddingItem(name='Wedding Chair', hcr=3, sell_price=500),
     WeddingItem(name='Wedding Flower Stand', hcr=4, sell_price=1000),
-    WeddingItem(name='Wedding Head Table', hcr=6, sell_price=750),
     WeddingItem(name='Wedding Pipe Organ', hcr=40, sell_price=25000),
-    WeddingItem(name='Wedding Pumps', hcr=6, sell_price=750),
-    WeddingItem(name='Wedding Shoes', hcr=6, sell_price=750),
     WeddingItem(name='Wedding Table', hcr=6, sell_price=1500),
-    WeddingItem(name='Wedding Tuxedo', hcr=20, sell_price=3000),
-    WeddingItem(name='Cake Dress', hcr=20, sell_price=5000),
 ]
