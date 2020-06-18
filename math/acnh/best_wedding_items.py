e00ebd409ec1fda53ff5c9a6be5a61b68b895ff7
ashleawalker29 <ashleawalker29@gmail.com>
2020-06-18 13:45:38 -0600

    Altered get_best_wedding_items logic.
    Fixed math formulas to evaluate properly and subtract 'used' heart crystals.
    
    Altered wedding_items sorting to be more efficient with heart crystals. It now
    returns the minimum amount of items for the maximum amount of profit. The
    efficiency is essential due to the fact that a user can only trade for 8
    different items a day. The evaluation will not take this into account since
    the user can go on different days to use up their saved heart cystals.

--- math/acnh/best_wedding_items.py
+++ math/acnh/best_wedding_items.py
@@ -9,14 +9,14 @@ def get_input(text):
 
 def get_best_wedding_items():
     wedding_items_by_ratio = sorted(
-        wedding_items, key=operator.attrgetter('ratio', 'name'), reverse=True)
+        wedding_items, key=operator.attrgetter('ratio', 'hcr', 'name'), reverse=True)
 
     user_input = get_input('Enter the number of heart crystals that you have to trade: ')
 
     best_items = {}
     for item in wedding_items_by_ratio:
         if user_input >= item.hcr:
-            best_items[item.name] = user_input % item.hcr
-            user_input = (user_input % item.hcr) * item.hcr
+            best_items[item.name] = user_input // item.hcr
+            user_input = user_input - ((user_input // item.hcr) * item.hcr)
 
     return best_items
