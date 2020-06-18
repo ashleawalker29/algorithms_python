15b1d06bd85b7083768d4cfbc6c723e6e3686407
ashleawalker29 <ashleawalker29@gmail.com>
2020-06-18 13:32:00 -0600

    Implemeted get_best_wedding_items.
    This sorts the list of wedding_items by ratio and then by name in descending order.
    It then evaluates the best items to trade with the number of heart crystals the user
    has input. These items are returned as a dictionary where the keys are the item
    names and the values are how many the user should trade for to get the maximum profit.

--- math/acnh/best_wedding_items.py
+++ math/acnh/best_wedding_items.py
@@ -1,7 +1,22 @@
+import operator
+
+from WeddingItems import wedding_items
+
+
 def get_input(text):
     return input(text)
 
 
 def get_best_wedding_items():
-    user_input = get_input('Enter heart crystals')
-    return None
+    wedding_items_by_ratio = sorted(
+        wedding_items, key=operator.attrgetter('ratio', 'name'), reverse=True)
+
+    user_input = get_input('Enter the number of heart crystals that you have to trade: ')
+
+    best_items = {}
+    for item in wedding_items_by_ratio:
+        if user_input >= item.hcr:
+            best_items[item.name] = user_input % item.hcr
+            user_input = (user_input % item.hcr) * item.hcr
+
+    return best_items
