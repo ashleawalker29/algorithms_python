7a53bb491ff26383a4dbdd18538057aefdb20cdb
Ashlea Walker <ashleawalker29@gmail.com>
2020-08-13 13:15:50 -0600

    Changed json file path.

--- .github/scripts/decrypt_superuser_secret.sh
+++ .github/scripts/decrypt_superuser_secret.sh
@@ -3,4 +3,4 @@
 # --batch to prevent interactive command
 # --yes to assume 'yes' for questions
 gpg2 --quiet --batch --yes --decrypt --passphrase='$SUPERUSER_SECRET' \
---pinentry loopback --output $HOME/spreadsheet_manipulation/superuser_secret.json .github/scripts/superuser_secret.json.gpg
+--pinentry loopback --output spreadsheet_manipulation/superuser_secret.json .github/scripts/superuser_secret.json.gpg
