f1f89034cda46f9a513b45b65f2f6574aaa0d770
Ashlea Walker <ashleawalker29@gmail.com>
2020-08-13 12:38:56 -0600

    Moved superuser secrets files to a more appropriate area.
    Added the unencrypted superuser_secret.json file to .gitignore
    to prevent accidentally committing the unencrypted file.

--- .github/scripts/decrypt_superuser_secret.sh
+++ .github/scripts/decrypt_superuser_secret.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-# Decrypt the file
-mkdir $HOME/secrets
-
 # --batch to prevent interactive command
 # --yes to assume 'yes' for questions
 gpg --quiet --batch --yes --decrypt --passphrase='$SUPERUSER_SECRET' \
---output $HOME/spreadsheet_manipulation/superuser_secret.json superuser_scret.json.gpg
+--output $HOME/spreadsheet_manipulation/superuser_secret.json superuser_secret.json.gpg
