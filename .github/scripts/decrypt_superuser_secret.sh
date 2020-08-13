5f695b7faac25013da50f443a72c2ce77d34ab1b
Ashlea Walker <ashleawalker29@gmail.com>
2020-08-13 12:49:28 -0600

    Specified path for binary file.

--- .github/scripts/decrypt_superuser_secret.sh
+++ .github/scripts/decrypt_superuser_secret.sh
@@ -3,4 +3,4 @@
 # --batch to prevent interactive command
 # --yes to assume 'yes' for questions
 gpg --quiet --batch --yes --decrypt --passphrase='$SUPERUSER_SECRET' \
---output $HOME/spreadsheet_manipulation/superuser_secret.json superuser_secret.json.gpg
+--output $HOME/spreadsheet_manipulation/superuser_secret.json .github/scripts/superuser_secret.json.gpg
