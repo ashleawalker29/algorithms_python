6ac7d7e01d7fba9ad48f4ffaeb9b035a3dd9a3e5
Ashlea Walker <ashleawalker29@gmail.com>
2020-08-13 20:45:19 -0600

    Created 'Get All Needed Cards' test.
    This test evaluates the worksheets that apply to the 'Needed Cards'
    data. It tests that only the appropriate 5 worksheets (S1 Need, S2
    Need, S3 Need, S4 Need, and WA Need) are evaluated.
    
    The test also ensures that each worksheet has the same keys for
    consistent comparisons to be made between all 5 of the worksheets.
    
    Finally, the test tests whether or not there are 3 'needed' cards
    that have been found.
    
    NOTE: This test will require future changes as the 3 remaining cards
    are found.

--- spreadsheet_manipulation/test_sheet_manipulation.py
+++ spreadsheet_manipulation/test_sheet_manipulation.py
@@ -1,6 +1,7 @@
 import unittest
 
-from sheet_manipulation import get_all_keys, get_worksheet_names, open_worksheet
+from sheet_manipulation import (get_all_keys, get_all_needed_cards, get_worksheet_names,
+                                open_worksheet)
 
 
 class SheetManipulationTests(unittest.TestCase):
@@ -22,3 +23,23 @@ class SheetManipulationTests(unittest.TestCase):
         for worksheet_name in self.worksheet_names:
             opened_worksheet = open_worksheet(worksheet_name)
             self.assertTrue(get_all_keys(opened_worksheet))
+
+    def test_get_all_needed_cards(self):
+        NEEDED_WORKSHEET_KEYS = ['Card Number', 'Card Character', 'Have', 'Need']
+
+        needed_card_worksheets = [open_worksheet(worksheet_name) for worksheet_name in
+                                  self.worksheet_names if 'Need' in worksheet_name]
+
+        # 5 needed worksheets: S1 Need, S2 Need, S3 Need, S4 Need, and WA Need.
+        self.assertEqual(len(needed_card_worksheets), 5)
+
+        needed_cards = []
+        for needed_card_worksheet in needed_card_worksheets:
+            # Ensure that each worksheet has the same keys for consistent comparisons.
+            self.assertEqual(get_all_keys(needed_card_worksheet), NEEDED_WORKSHEET_KEYS)
+
+            needed_cards.extend(get_all_needed_cards(needed_card_worksheet))
+
+        # This test only works if there are 3 needed cards. This test will need to be changed once
+        # more/all cards have been obtained. See README for more information.
+        self.assertEqual(len(needed_cards), 3)
