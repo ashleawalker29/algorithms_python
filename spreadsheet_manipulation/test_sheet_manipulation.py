ab5f33911a9fa87d6ba908fe5061a5c130a04ade
Ashlea Walker <ashleawalker29@gmail.com>
2020-08-13 19:48:32 -0600

    Created a 'setUp' method for the Sheet Manipulation Tests.
    Removed repetitive code and consolidated it within the new 'setUp'
    method. This will initialize the list of worksheet names before
    every test.

--- spreadsheet_manipulation/test_sheet_manipulation.py
+++ spreadsheet_manipulation/test_sheet_manipulation.py
@@ -5,23 +5,20 @@ from sheet_manipulation import get_all_keys, get_worksheet_names, open_worksheet
 
 class SheetManipulationTests(unittest.TestCase):
 
+    def setUp(self):
+        self.worksheet_names = get_worksheet_names()
+
     def test_get_worksheet_names(self):
-        worksheet_names = get_worksheet_names()
+        self.assertEqual(len(self.worksheet_names), 10)
 
-        self.assertEqual(len(worksheet_names), 10)
-
-        for worksheet_name in worksheet_names:
+        for worksheet_name in self.worksheet_names:
             self.assertIsInstance(worksheet_name, str)
 
     def test_open_worksheets(self):
-        worksheet_names = get_worksheet_names()
-
-        for worksheet_name in worksheet_names:
+        for worksheet_name in self.worksheet_names:
             self.assertTrue(open_worksheet(worksheet_name))
 
     def test_get_all_keys(self):
-        worksheet_names = get_worksheet_names()
-
-        for worksheet_name in worksheet_names:
+        for worksheet_name in self.worksheet_names:
             opened_worksheet = open_worksheet(worksheet_name)
             self.assertTrue(get_all_keys(opened_worksheet))
