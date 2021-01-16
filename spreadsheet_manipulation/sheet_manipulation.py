38c3817bdca8c5216da17e230b27f8b0b8e91cd0
Ashlea Walker <ashleawalker29@gmail.com>
2021-01-15 17:00:50 -0700

    Add absolute path reference for superuser_secret file.

--- spreadsheet_manipulation/sheet_manipulation.py
+++ spreadsheet_manipulation/sheet_manipulation.py
@@ -2,7 +2,8 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 SCOPES = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-CREDENTIALS = Credentials.from_service_account_file('superuser_secret.json', scopes=SCOPES)
+CREDENTIALS = Credentials.from_service_account_file(
+    './spreadsheet_manipulation/superuser_secret.json', scopes=SCOPES)
 CLIENT = gspread.authorize(CREDENTIALS)
 
 SHEET_NAME = 'AC Amiibo Cards Trading'
